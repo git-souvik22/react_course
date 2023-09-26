3606fe9b759aaafb0e6a7d1a9f089ea7c7f67e96
Souvik Roy <125180450+git-souvik22@users.noreply.github.com>
2023-09-26 20:00:11 +0530

    passing-function-props

--- src/App.js
+++ src/App.js
@@ -4,9 +4,12 @@ import Child from "./Child";
 
 const App = () => {
   const [name, setName] = useState("Souvik");
+  const changeName = () => {
+    setName(name === "Souvik" ? "Developer" : "Souvik");
+  };
   return (
     <div className="App">
-      <Child name={name} setName={setName} />
+      <Child name={name} changeName={changeName} />
     </div>
   );
 };
--- src/Child.js
+++ src/Child.js
@@ -1,12 +1,12 @@
 import React from "react";
 
-const Child = ({ name, setName }) => {
+const Child = ({ name, changeName }) => {
   return (
     <div>
       <h1>{name}</h1>
       <button
         className="btn btn-outline-dark fs-3"
-        onClick={() => setName(name === "Souvik" ? "Developer" : "Souvik")}
+        onClick={() => changeName()}
       >
         Change Name
       </button>
