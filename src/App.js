5328f7810f61edc131deab7f4fc385781f61e2f8
Souvik Roy <125180450+git-souvik22@users.noreply.github.com>
2023-09-26 19:55:06 +0530

    child-setState-pass

--- src/App.js
+++ src/App.js
@@ -6,13 +6,7 @@ const App = () => {
   const [name, setName] = useState("Souvik");
   return (
     <div className="App">
-      <Child name={name} />
-      <button
-        className="btn btn-outline-dark fs-3"
-        onClick={() => setName("Developer")}
-      >
-        Change Name
-      </button>
+      <Child name={name} setName={setName} />
     </div>
   );
 };
--- src/Child.js
+++ src/Child.js
@@ -1,9 +1,15 @@
 import React from "react";
 
-const Child = ({ name }) => {
+const Child = ({ name, setName }) => {
   return (
     <div>
       <h1>{name}</h1>
+      <button
+        className="btn btn-outline-dark fs-3"
+        onClick={() => setName("Developer")}
+      >
+        Change Name
+      </button>
     </div>
   );
 };
