78e10fb994756c3e4fc3c4b290619a9bf922008b
Souvik Roy <125180450+git-souvik22@users.noreply.github.com>
2024-03-28 18:47:44 +0530

    useState hook in react

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
-const Button = () => {
-  const handleClick = (a, b) => {
-    console.log(a + b);
+import { useState } from "react";
+
+const Counter = () => {
+  const [num, setNum] = useState(0);
+
+  const inc = () => {
+    if (num < 20) {
+      setNum(num + 1);
+    }
   };
-  return <button onClick={() => handleClick(2, 2)}>Click</button>;
+
+  const dec = () => {
+    if (num > 0) {
+      setNum(num - 1);
+    }
+  };
+
+  return (
+    <>
+      <h2>{num}</h2>
+      <button onClick={inc}>+</button>
+      <button onClick={dec}>-</button>
+    </>
+  );
 };
 
 const App = () => {
-  return (
-    <>
-      <Button />
-    </>
-  );
+  return <Counter />;
 };
 
 export default App;
