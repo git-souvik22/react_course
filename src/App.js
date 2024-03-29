7de01b1b9d3f1616d8798780e84429dc8e332bbc
Souvik Roy <125180450+git-souvik22@users.noreply.github.com>
2024-03-29 18:40:51 +0530

    basic form data using useState hook

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
 import { useState } from "react";
 
 const App = () => {
-  const [movies, setMovies] = useState([
-    { id: 1, title: "Spiderman", ratings: 4 },
-    { id: 2, title: "Superman", ratings: 5 },
-  ]);
+  const [username, setUsername] = useState("");
 
-  const handleClick = () => {
-    setMovies(
-      movies.map((mov) => (mov.id === 1 ? { ...mov, title: "Avatar" } : mov))
-    );
+  const handleChange = (event) => {
+    setUsername(event.target.value);
+    // console.log(event.target.value);
   };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert(`You typed: ${username}`);
+    setUsername("");
+  };
+
   return (
     <>
-      {movies.map((movie) => (
-        <li key={movie.id}>{movie.title}</li>
-      ))}
-      <button onClick={handleClick}>Change Name</button>
+      <h1>Demo Form</h1>
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={username} onChange={handleChange} />
+        <button type="submit">Submit</button>
+      </form>
     </>
   );
 };
