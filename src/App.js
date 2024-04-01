903c1a8089d253b63bcef92f8a52da46d57e181c
Souvik Roy <125180450+git-souvik22@users.noreply.github.com>
2024-04-01 13:25:40 +0530

    installed axios library

--- src/App.js
+++ src/App.js
@@ -1,35 +1,9 @@
-import { useState } from "react";
+import Main from "./Main";
+
 const App = () => {
-  const [input, setInput] = useState("");
-  const [todos, setTodos] = useState([]);
-  const generateID = () => {
-    let index = todos.length;
-    return (index += 1);
-  };
-  const addTodo = () => {
-    setTodos([...todos, { text: input, id: generateID() }]);
-    setInput("");
-  };
-  const removeTodo = (id) =>
-    setTodos((todos) => todos.filter((t) => t.id !== id));
   return (
     <>
-      <input
-        type="text"
-        value={input}
-        onChange={(e) => setInput(e.target.value)}
-        placeholder="New Todo"
-      />
-      <button onClick={() => addTodo()}>ADD</button>
-      <h2>Todo list :-</h2>
-      <ul>
-        {todos.map(({ text, id }) => (
-          <li key={id}>
-            <span>{text}</span>
-            <button onClick={() => removeTodo(id)}>❌</button>
-          </li>
-        ))}
-      </ul>
+      <Main />
     </>
   );
 };
---
+++ src/Main.jsx
@@ -0,0 +1,11 @@
+// import { useState } from "react";
+
+const Main = () => {
+  return (
+    <section>
+      <h1>Hi</h1>
+    </section>
+  );
+};
+
+export default Main;
